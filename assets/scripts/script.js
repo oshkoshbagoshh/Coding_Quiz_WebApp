4564194b8d2566690ac6a62a49a62f7756b6fb22
AJ Javadi <amirjavadi25@gmail.com>
2023-05-23 22:57:37 -0500

    made updates to js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -53,95 +53,131 @@ var startBtn = document.getElementById('start-btn');
 var timer = document.getElementById("timer");
 var count = 75;
 var clock = 75;
-var questions = ["Question One","Question Two", "Question Three","Question Four"];
-var answers = [1,2,3,4];
+var questions = ["Question One", "Question Two", "Question Three", "Question Four"];
+var answers = [1, 2, 3, 4];
 var wins = 0;
 var losses = 0;
 var score = 0;
-
+var timerID;
 // Event listener
 function handleClick() {
     console.log('Button clicked!');
     // log to the console
     //now do something
-    alert("click working");
+    // alert("click working");
+    getQuestion()
+    // use setTimeout as part of web API
 
-  }
-  
-  // Attach event listener
-  startBtn.addEventListener('click', function(e){
+    timerID = setInterval(clockTick, 1000); // 1000 ms 
+
+}
+
+function clockTick() {
     count--;
     timer.textContent = count;
-    
-    // count down 
-    if (count <== 0) {
+};
 
-        clearInterval(timer)
-        h2E1.textContent = "times up!"
-    } else () {
-        //do something else
-        timer.textContent = "count"
-    }
-    
-    
-    }
-  }
+// Attach event listener
+startBtn.addEventListener('click', handleClick);
+
+
+// // count down 
+// if (count <== 0) {
+
+//     clearInterval(timer)
+//     h2E1.textContent = "times up!"
+// } else if (count ) {
+//     //do something else
+//     timer.textContent = "count"
+// }
+
 
 
 //   Questions:
 
 // Questionlist array of objects 
-var questionList =  [
-    {
-        questionOne: "Commonly used data types DO Not Include:",
-        a: "1. Strings",
-        b: "2. Booleans",
-        c: "3. Alerts",
-        d: "4. Numbers",
-    },
-        {
-            questionTwo: "The condition in an if/else statement is enclosed with ____.",
-            a:"1. Quotes",
-            b:"2. Curly Brackets",
-            c:"3. Parenthesis",
-            d:"4. Square Brackets",
+// var questionList = [
+//     {
+//         questionOne: "Commonly used data types DO Not Include:",
+//         a: "1. Strings",
+//         b: "2. Booleans",
+//         c: "3. Alerts",
+//         d: "4. Numbers",
+//     },
+//     {
+//         questionTwo: "The condition in an if/else statement is enclosed with ____.",
+//         a: "1. Quotes",
+//         b: "2. Curly Brackets",
+//         c: "3. Parenthesis",
+//         d: "4. Square Brackets",
+//     },
+
+//     {
+//         questionThree: "Arrays in JavaScript can be used to store _____",
+//         a: "1. Numbers and strings ",
+//         b: "2. Other Arrays",
+//         c: "3. booleans",
+//         d: "4. all of the above",
+
+//     },
+
+//     {
+//         questionFour: "String values must be enclosed within ___ when being assigned to variables",
+//         a: "1. commas",
+//         b: "2. curly brackets",
+//         c: "3. quotes ",
+//         d: "4. parenthesis ",
+
+//     },
+//     {
+//         questionFive: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+//         a: "1. JavaScript",
+//         b: "2. terminal/bash",
+//         c: "3. for loops",
+//         d: "console.log",
+//     }
 
 
-
-
-        },
-
-        {
-            questionThree:"Arrays in JavaScript can be used to store _____",
-            a:"1. Numbers and strings " ,
-            b:"2. Other Arrays",
-            c:"3. booleans",
-            d:"4. all of the above",
-
-        },
-
-        {  questionFour:"String values must be enclosed within ___ when being assigned to variables",
-            a:"1. commas" ,
-            b:"2. curly brackets",
-            c:"3. quotes ",
-            d:"4. parenthesis ",
-
-    },
-    {
-        questionFive:"A very useful tool used during development and debugging for printing content to the debugger is: ",
-        a:"1. JavaScript",
-        b:"2. terminal/bash",
-        c:"3. for loops",
-        d:"console.log",
-    }
-
-
-];
+// ];
 // log to the console
-console.log(questionList);
-
-var stringQuestionlist = questionList.toString();
-console.log(stringQuestionlist);
+// console.log(questionList);
+// array for questions
+var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses',
+    },
+    // {
+    //   title: 'Arrays in JavaScript can be used to store ____.',
+    //   choices: [
+    //     'numbers and strings',
+    //     'other arrays',
+    //     'booleans',
+    //     'all of the above',
+    //   ],
+    //   answer: 'all of the above',
+    // },
+    {
+      title:
+        'String values must be enclosed within ____ when being assigned to variables.',
+      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+      answer: 'quotes',
+    },
+    {
+      title:
+        'A very useful tool used during development and debugging for printing content to the debugger is:',
+      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      answer: 'console.log',
+    },
+  ];
+// var stringQuestionlist = questionList.toString();
+// console.log(stringQuestionlist);
 
 // create function to check answers
 
@@ -149,15 +185,60 @@ console.log(stringQuestionlist);
 var quiz = document.getElementById(".quiz");
 const answerElements = document.querySelectorAll(".answer");
 var questionElement = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
+// const a_text = document.getElementById("a_text");
+// const b_text = document.getElementById("b_text");
+// const c_text = document.getElementById("c_text");
+// const d_text = document.getElementById("d_text");
 
 // let currentQuiz = 0;
 // let score = 0;
-
+var currentQuestionIndex = 0;
+var choicesContainer = document.getElementById('choices'); 
 // function check_Answers () {}
+function getQuestion () {
 
+    var currentQuestion = questions[currentQuestionIndex]; //use the index 
+    var titleElement = document.getElementById("questionTitle");
+    titleElement.textContent = currentQuestion.title;
+    var choicesArray = currentQuestion.choices;
 
+    //clear out contents
+    choicesContainer.innerHTML="";
+  console.log("choices array", choicesArray);
+    for (var i = 0;i<choicesArray.length;i++) {
+      // create buttons
+      var choiceEl = document.createElement("button")
+      var choice = choicesArray[i]; 
+      choiceEl.textContent = choice;
+      choiceEl.setAttribute("value", choice);
+      choicesContainer.appendChild(choiceEl);
 
+    }
+    
+
+}
+
+choicesContainer.addEventListener("click",questionClick);
+function questionClick(event) {
+var buttonEl = event.target
+console.log("buttonElement",buttonEl);
+var userAnswer = buttonEl.value;
+
+// if statement to check against user input
+
+if (userAnswer !== questions[currentQuestionIndex].answer) {
+  // 
+  count=count-15; //take away 15 seconds 
+
+} 
+currentQuestionIndex++;
+// add logic here !
+// add endquizlogic here
+// clearInterval
+getQuestion();
+
+//create end quiz function where they can save their score to the local storage
+//check when you run out of questions or  when time is up you
+
+}
+// use inns
